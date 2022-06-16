22860b5ddc71c7d170b8a81a98cb5d3d0e628909
luizpaulomw <55301155+luizpaulomw@users.noreply.github.com>
2022-06-15 23:43:24 -0300

    backend funcionando

--- realizar-cadastro.php
+++ realizar-cadastro.php
@@ -82,19 +82,4 @@ require_once './conexao.php';
 	echo 'nome já Cadastrado!';
 }
 
-
-
-
-
-    //  try {
-    //     $conn = new PDO("mysql:host=$host;dbname=$dbname",  $username, $password);
-    //     // set the PDO error mode to exception
-    //     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //     echo "Connected successfully";
-    //   } catch(PDOException $e) {
-    //     echo "Connection failed: " . $e->getMessage();
-    //   }
-
-
-
 ?>
